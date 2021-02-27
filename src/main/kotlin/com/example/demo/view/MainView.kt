572ee8af70d8cf06d9fa65d0a9b6fb775799f8e0
Evgeniy <284661@niuitmo.ru>
2021-02-28 02:07:06 +0300

    test view added

--- src/main/kotlin/com/example/demo/view/MainView.kt
+++ src/main/kotlin/com/example/demo/view/MainView.kt
@@ -1,6 +1,7 @@
 package com.example.demo.view
 
 import com.example.demo.controller.DichotomyViewIterator
+import com.example.demo.model.base.Graph
 import javafx.scene.chart.LineChart
 import javafx.scene.chart.NumberAxis
 import tornadofx.*
@@ -8,47 +9,72 @@ import java.util.*
 
 class SelectionMethodEvent(val iter: DichotomyViewIterator) : FXEvent()
 
-class NextIterationEvent() : FXEvent()
+class NextIterationEvent(val graphs: List<Graph>) : FXEvent()
 
+class GraphView() : Fragment() {
+    override val root = hbox {
+    }
+    fun getChart(graphs: List<Graph>): LineChart<Number, Number> = run {
+        var xmax = -1e9
+        var xmin = 1e9
+        var ymax = -1e9
+        var ymin = 1e9
+        graphs.forEach {
+            it.points.forEach {
+                xmax = Math.max(xmax, it.x)
+                xmin = Math.min(xmin, it.x)
+                ymax = Math.max(ymax, it.y)
+                ymin = Math.min(ymin, it.y)
+            }
+        }
+        linechart(graphs.toString(), NumberAxis(xmin, xmax,(xmax - xmin) / 10.0), NumberAxis(ymin, ymax, (ymax - ymin) / 10.0)) {
+            graphs.forEach {
+                series(it.toString()) {
+                    it.points.forEach {
+                        data(it.x, it.y)
+                    }
+                }
+            }
+        }
+    }
+}
 
 class MainView : View("Hello TornadoFX") {
     val controller: MethodController by inject()
     var iterator: DichotomyViewIterator? = null
+    var graphView = GraphView()
     override val root = vbox {
         combobox(values = controller.methods) {
             setOnAction {
                 fire(SelectionMethodEvent(selectionModel.selectedItem))
             }
         }
+        add(graphView)
         subscribe<SelectionMethodEvent> {
             iterator = it.iter
-            fire(NextIterationEvent())
-            linechart(iterator.toString(), NumberAxis(0.0, 100.0, 10.0), NumberAxis()) {
-                subscribe<NextIterationEvent> {
-                    if (iterator == null || !iterator!!.hasNext()) {
-
-                    } else {
-                        xAxis
-                        data.clear()
-                        iterator!!.next().forEach {
-                            series(it.toString()) {
-                                it.points.forEach {
-                                    data(it.x, it.y)
-                                }
-                            }
-                        }
-                    }
-                }
+            if (iterator!!.hasNext()) {
+                fire(NextIterationEvent(iterator!!.next()))
             }
         }
         button("next") {
-            setOnAction {
-                fire(NextIterationEvent())
+            action {
+                fire(NextIterationEvent(iterator!!.next()))
             }
+        }
+        subscribe<NextIterationEvent> {
+            println(it.graphs)
+            graphView.root.clear()
+            val chart = graphView.getChart(it.graphs)
+            chart.createSymbols = false
+            graphView.root.add(
+                hbox {
+                    add(chart)
+                }
+            )
         }
     }
 }
 
 class MethodController: Controller() {
-    val methods = listOf(DichotomyViewIterator(0.0, Math.PI * 2.0, 1e-9, 1e-10))
+    val methods = listOf(DichotomyViewIterator(0.0, Math.PI * 2.0, 1e-5, 1e-6), DichotomyViewIterator(0.0, Math.PI * 2.0, 1e-9, 1e-10))
 }
