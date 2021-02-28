62e2538c239d8344b3ab314eee0d113895dc99cb
Evgeniy <284661@niuitmo.ru>
2021-02-28 16:28:31 +0300

    sizes partially fixed size partially fixed

--- src/main/kotlin/com/example/demo/app/Styles.kt
+++ src/main/kotlin/com/example/demo/app/Styles.kt
@@ -1,21 +1,32 @@
 package com.example.demo.app
 
+import javafx.scene.paint.Color
 import javafx.scene.text.FontWeight
-import tornadofx.Stylesheet
-import tornadofx.box
-import tornadofx.cssclass
-import tornadofx.px
+import tornadofx.*
 
 class Styles : Stylesheet() {
     companion object {
         val heading by cssclass()
+        val chartBox by cssclass()
     }
 
     init {
         root {
+            minWidth = 1400.px
+            minHeight = 700.px
+        }
+        root {
             padding = box(10.px)
             fontSize = 20.px
             fontWeight = FontWeight.BOLD
+        }
+        chartBox {
+            minWidth = 1000.px
+        }
+        chartLineSymbol {
+            padding = box(1.px)
+//            backgroundRadius = multi(box(100.px))
+//            backgroundColor = multi(Color.RED)
         }
     }
 }
--- src/main/kotlin/com/example/demo/view/MainView.kt
+++ src/main/kotlin/com/example/demo/view/MainView.kt
@@ -1,7 +1,9 @@
 package com.example.demo.view
 
+import com.example.demo.app.Styles
 import com.example.demo.controller.DichotomyViewIterator
 import com.example.demo.model.base.Graph
+import com.example.demo.model.base.Point
 import javafx.scene.chart.LineChart
 import javafx.scene.chart.NumberAxis
 import tornadofx.*
@@ -11,45 +13,63 @@ class SelectionMethodEvent(val iter: DichotomyViewIterator) : FXEvent()
 
 class NextIterationEvent(val graphs: List<Graph>) : FXEvent()
 
-class GraphView() : Fragment() {
-    override val root = hbox {
-    }
-    fun getChart(graphs: List<Graph>): LineChart<Number, Number> = run {
-        var xmax = -1e9
-        var xmin = 1e9
-        var ymax = -1e9
-        var ymin = 1e9
-        graphs.forEach {
-            it.points.forEach {
-                xmax = Math.max(xmax, it.x)
-                xmin = Math.min(xmin, it.x)
-                ymax = Math.max(ymax, it.y)
-                ymin = Math.min(ymin, it.y)
-            }
-        }
-        linechart(graphs.toString(), NumberAxis(xmin, xmax,(xmax - xmin) / 10.0), NumberAxis(ymin, ymax, (ymax - ymin) / 10.0)) {
-            graphs.forEach {
-                series(it.toString()) {
-                    it.points.forEach {
-                        data(it.x, it.y)
-                    }
+class MainView : View("huy TornadoFX") {
+    val controller: MethodController by inject()
+    var iterator: DichotomyViewIterator? = null
+
+    fun getChart(graphs: List<Graph>): LineChart<Number, Number>.() -> Unit = {
+        graphs.map {
+            series(it.toString()) {
+                it.points.forEach {
+                    data(it.x, it.y)
                 }
             }
         }
     }
-}
 
-class MainView : View("Hello TornadoFX") {
-    val controller: MethodController by inject()
-    var iterator: DichotomyViewIterator? = null
-    var graphView = GraphView()
-    override val root = vbox {
+    fun getXAxis(graphs: List<Graph>): NumberAxis = run {
+        val left = graphs.foldRight<Graph, Double>(100.0) { el, acc ->
+            val min = el.points.foldRight<Point, Double>(100.0) { point, pacc ->
+                if (point.x < pacc) point.x else pacc
+            }
+            if (min < acc) min else acc
+        }
+        val right = graphs.foldRight<Graph, Double>(-100.0) { el, acc ->
+            val max = el.points.foldRight<Point, Double>(-100.0) { point, pacc ->
+                if (point.x > pacc) point.x else pacc
+            }
+            if (max > acc) max else acc
+        }
+        NumberAxis(left, right, (right - left) / 15.0)
+    }
+
+    fun getYAxis(graphs: List<Graph>): NumberAxis = run {
+        val left = graphs.foldRight<Graph, Double>(100.0) { el, acc ->
+            val min = el.points.foldRight<Point, Double>(100.0) { point, pacc ->
+                if (point.y < pacc) point.y else pacc
+            }
+            if (min < acc) min else acc
+        }
+        val right = graphs.foldRight<Graph, Double>(-100.0) { el, acc ->
+            val max = el.points.foldRight<Point, Double>(-100.0) { point, pacc ->
+                if (point.y > pacc) point.y else pacc
+            }
+            if (max > acc) max else acc
+        }
+        NumberAxis(left, right, (right - left) / 15.0)
+    }
+
+    override val root = hbox {
+        addClass(Styles.chartBox)
         combobox(values = controller.methods) {
             setOnAction {
                 fire(SelectionMethodEvent(selectionModel.selectedItem))
             }
         }
-        add(graphView)
+        linechart("none", NumberAxis(), NumberAxis()) {
+            id = "hueta"
+            addClass(Styles.chartBox)
+        }
         subscribe<SelectionMethodEvent> {
             iterator = it.iter
             if (iterator!!.hasNext()) {
@@ -62,15 +82,13 @@ class MainView : View("Hello TornadoFX") {
             }
         }
         subscribe<NextIterationEvent> {
-            println(it.graphs)
-            graphView.root.clear()
-            val chart = graphView.getChart(it.graphs)
-            chart.createSymbols = false
-            graphView.root.add(
-                hbox {
-                    add(chart)
-                }
-            )
+            this@hbox.children.asSequence().filter { it.id == "hueta" }.toList()[0].removeFromParent()
+            val chart = linechart(iterator.toString(), getXAxis(it.graphs), getYAxis(it.graphs)) {
+                id = "hueta"
+                addClass(Styles.chartBox)
+            }
+            chart.(getChart(it.graphs))()
+            this@hbox.add(chart)
         }
     }
 }
